0b92cc0e2d10b22025c3e7cc072cae170a81d44e
Malwin Niehus <malwin.niehus@gmx.de>
2020-06-04 18:12:56 +0200

    setuptools + CI

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='python_helpers',
+    version='0.1',
+    packages=find_packages(),
+    author='Malwin Niehus',
+    author_email='niehus@hiskp.uni-bonn.de',
+    description='some short helpers to simplify common tasks',
+    keywords='enum string json',
+    url='https://github.com/patbat/python_helpers',
+    project_urls={
+        'Source Code': 'https://github.com/patbat/python_helpers',
+    },
+    classifiers=[
+        'License :: MIT License'
+    ]
+)
