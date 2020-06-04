70a26e6a754d7630f531c5120e1b7320107f8ef1
Malwin Niehus <malwin.niehus@gmx.de>
2020-06-04 17:55:08 +0200

    enumeration that goes hand in hand with strings

---
+++ python_helpers/enumeration.py
@@ -0,0 +1,38 @@
+"""
+==================================================
+An enumeration that goes hand in hand with strings
+==================================================
+"""
+from __future__ import annotations
+import enum
+
+
+class StringEnum(enum.IntEnum):
+    """An enumeration that goes hand in hand with strings.
+
+    It allows for construction from strings and comparison with strings.
+    For an example, see `python_helpers.tests.test_enumeration`."""
+    @classmethod
+    def allowed_values(cls) -> str:
+        """Return the allowed str values."""
+        return ", ".join(x.name for x in cls)
+
+    @classmethod
+    def from_str(cls, string: str) -> StringEnum:
+        """Generate an instance corresponding to `string`."""
+        for value in cls:
+            if string == value.name:
+                return value
+        raise ValueError(f"Unknown value. Needs to be one of"
+                         f" {cls.allowed_values()}.")
+
+    def __str__(self):
+        return self.name
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            other = type(self).from_str(other)
+        return super().__eq__(other)
+
+    def __hash__(self):
+        return hash(self.name)
---
+++ python_helpers/tests/test_enumeration.py
@@ -0,0 +1,27 @@
+import pytest
+
+from python_helpers.enumeration import StringEnum
+
+
+class Three(StringEnum):
+    one = 1
+    two = 2
+    three = 3
+
+
+def test_eq():
+    one, two, _ = Three
+    assert two == 'two'
+    assert 'two' == two
+    assert two == two
+    assert two != one
+    assert two != 'one'
+    assert 'one' != two
+
+
+def test_construction():
+    assert Three.three is Three.from_str('three')
+
+
+def test_allowed_values():
+    return Three.allowed_values == 'one, two, three'
